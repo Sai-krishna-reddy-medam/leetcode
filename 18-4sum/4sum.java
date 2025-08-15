dab35b253b71707660e18d5251b3c00c1b27ca85
MEDAM VENKATA SAI KRISHNA REDDY <saikrishnamedam558@gmail.com>
2025-08-15 23:13:23 +0530

    Time: 5 ms (95.25%) | Memory: 44 MB (62.88%) - LeetSync

---
+++ 18-4sum/4sum.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 4) {
+            return result;
+        }
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            long minSum1 = (long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3];
+            if (minSum1 > target) {
+                break;
+            }
+            long maxSum1 = (long)nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3];
+            if (maxSum1 < target) {
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+                    long currentSum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if (currentSum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (currentSum < target) {
+                        left++;
+                    } else { 
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
